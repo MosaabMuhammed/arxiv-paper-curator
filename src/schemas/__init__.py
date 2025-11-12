5f91c47ce5e3a669ffac9ece67922c8a7abd6476
mosaab <mosaab@dxwand.com>
2025-11-12 16:25:55 +0200

    Add ask router and schemas for question answering functionality

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,3 +1,4 @@
+from .ask_schema import AskRequest, AskResponse, PaperSource
 from .health_schema import HealthResponse, ServiceStatus
 from .paper_schema import PaperCreate, PaperResponse, PaperSearchResponse
 
